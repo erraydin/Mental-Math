b32bad7b5b71c4b5a5619888f51eb3946920337e
Eray Aydin <erraydin@gmail.com>
2021-05-03 05:03:11 -0500

    test data binding

--- app/src/main/java/com/erraydin/mentalmath/MainActivity.kt
+++ app/src/main/java/com/erraydin/mentalmath/MainActivity.kt
@@ -2,10 +2,16 @@ package com.erraydin.mentalmath
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.erraydin.mentalmath.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Eray", "Aydin")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
         setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/erraydin/mentalmath/MyName.kt
@@ -0,0 +1,3 @@
+package com.erraydin.mentalmath
+
+data class MyName(var firstName: String = "", var lastName: String = "")
