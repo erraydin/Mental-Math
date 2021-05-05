f56933c5a0bf09c5e593df31d9f96869b8bf294b
Eray Aydin <erraydin@gmail.com>
2021-05-05 04:35:38 -0500

    fix layouts

--- app/src/main/java/com/erraydin/mentalmath/screens/title/TitleFragment.kt
+++ app/src/main/java/com/erraydin/mentalmath/screens/title/TitleFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.erraydin.mentalmath.R
 import com.erraydin.mentalmath.databinding.FragmentTitleBinding
 
@@ -18,9 +18,13 @@ class TitleFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
-        binding.buttonStartGame.setOnClickListener {    view ->
-            view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+
+        binding.buttonStartGame.setOnClickListener {
+            findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
         }
+//        binding.buttonScoreHistory.setOnClickListener {
+//            findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToHistoryFragment())
+//        }
         return binding.root
     }
 
